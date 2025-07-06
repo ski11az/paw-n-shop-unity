4929f7d61fab61948403d444d5e75f8549edc4ea
Erica Näslund <dv24end@cs.umu.se>
2025-07-06 13:03:02 +0200

    Added storeflow logic

--- Assets/StoreFlow.cs
+++ Assets/StoreFlow.cs
@@ -4,24 +4,38 @@ using UnityEngine;
 
 public class StoreFlow : MonoBehaviour
 {
-    public GameObject nightScene;
     public GameObject customer1;
 
-    public int clickCount = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //startday
+        //Loop through all customers
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log("Kund nr" + i);
+            //Handles customers and minigames by calling customerActions
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            clickCount = 1;
-            nightScene.SetActive(false);
-        }
+
     }
 }
+
+
+//Create 
+/*public void customerActions(Gameobject customer)
+{
+    //Customer comes in
+    //Customer dialogue runs
+    //Camera pans to right
+    //Enters minigame scene and play minigame
+    //Exits minigame and´returns to scene
+    //Pans camera to left
+    //Run customer exit dialogue
+    //Customer exits the shop
+}*/
